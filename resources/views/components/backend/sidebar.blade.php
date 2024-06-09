bd41383418472da1d416a621d7e43699ed4b10f5
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2024-06-09 13:34:32 +0800

    add logo siberta 😍

--- resources/views/components/backend/sidebar.blade.php
+++ resources/views/components/backend/sidebar.blade.php
@@ -6,7 +6,7 @@
         </button>
         <h1 class="navbar-brand navbar-brand-autodark">
             <a href="{{ route('home') }}" class="px-3">
-                <img src="{{ asset('static/ryoogen.svg') }}" width="100%" height="100%" alt="Tabler">
+                <img src="{{ asset('assets/siberta/logo-siberta-light.svg') }}" width="100%" height="100%" alt="Tabler">
             </a>
         </h1>
         <div class="navbar-nav flex-row d-lg-none">
