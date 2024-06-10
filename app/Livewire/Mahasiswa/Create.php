cc95c970e3d2a6984f8168057073217f601539cf
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2024-06-10 20:17:37 +0800

    feat: add mahasiswa form ✅

--- app/Livewire/Mahasiswa/Create.php
+++ app/Livewire/Mahasiswa/Create.php
@@ -2,10 +2,68 @@
 
 namespace App\Livewire\Mahasiswa;
 
+use App\Models\Mahasiswa;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class Create extends Component
 {
+    public $namaMahasiswa;
+    public $nim;
+    public $nomorPonsel;
+    public $email;
+    public $tahunMasuk;
+    public $programStudi;
+    public $alamat;
+
+    public function rules(){
+        return [
+            'namaMahasiswa' => ['required','string','min:2','max:255'],
+            'nim' => ['required','string','min:2','unique:mahasiswa,nim'],
+            'nomorPonsel' => ['required','string','min:2','unique:mahasiswa,phone'],
+            'email' => ['required','string','min:2','max:255','email','unique:mahasiswa,email'],
+            'tahunMasuk' => ['required','string','min:2'],
+            'programStudi' => ['required','string','min:2','max:255'],
+            'alamat' => ['nullable','string','min:2'],
+        ];
+    }
+
+    public function save(){
+        $this->validate();
+
+        try{
+            DB::beginTransaction();
+
+            Mahasiswa::create([
+                'name' => $this->namaMahasiswa,
+                'nim' => $this->nim,
+                'phone' => $this->nomorPonsel,
+                'email' => $this->email,
+                'entry_year' => $this->tahunMasuk,
+                'program_studi' => $this->programStudi,
+                'address' => $this->alamat,
+            ]);
+
+            DB::commit();
+        }catch(Exception $e){
+            session()->flash('alert', [
+                'type' => 'danger',
+                'message' => 'Gagal.',
+                'detail' => "data mahasiswa gagal ditambah.",
+            ]);
+        }
+
+        session()->flash('alert', [
+            'type' => 'success',
+            'message' => 'Berhasil.',
+            'detail' => "data mahasiswa berhasil ditambah.",
+        ]);
+
+        return redirect()->route('mahasiswa.index');
+    }
+
     public function render()
     {
         return view('livewire.mahasiswa.create');
--- resources/views/livewire/mahasiswa/create.blade.php
+++ resources/views/livewire/mahasiswa/create.blade.php
@@ -25,12 +25,77 @@
                         label="Nama Mahasiswa"
                         placeholder="masukkan nama mahasiswa"
                         type="text"
+                        autofocus
+                        required
                     />
                 </div>
 
                 <div class="col-12 col-lg-6">
-                    <x-form.input wire:model="avatar" name="avatar" label="Avatar" placeholder="masukkan avatar"
-                        type="file" />
+                    <x-form.input
+                        wire:model="nim"
+                        name="nim"
+                        label="NIM"
+                        placeholder="masukkan nim mahasiswa"
+                        type="text"
+                        required
+                    />
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <x-form.input
+                        wire:model="nomorPonsel"
+                        name="nomorPonsel"
+                        label="Nomor Ponsel"
+                        placeholder="masukkan nomor ponsel"
+                        type="number"
+                        required
+                    />
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <x-form.input
+                        wire:model="email"
+                        name="email"
+                        label="Email"
+                        placeholder="masukkan email"
+                        type="text"
+                        required
+                    />
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <x-form.input
+                        wire:model="tahunMasuk"
+                        name="tahunMasuk"
+                        label="Tahun Masuk"
+                        placeholder="masukkan tahun masuk"
+                        type="number"
+                        required
+                    />
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <x-form.select
+                        wire:model="programStudi"
+                        name="programStudi"
+                        label="Program Studi"
+                        required
+                    >
+                        <option selected value=""> - Pilih Program Studi - </option>
+                        @foreach (config('const.program_studi') as $key => $programStudi)
+                            <option wire:key="row-{{ $programStudi }}" value="{{ $key }}">{{ ucwords($programStudi) }}
+                            </option>
+                        @endforeach
+                    </x-form.select>
+                </div>
+
+                <div class="col-12">
+                    <x-form.textarea
+                        wire:model="alamat"
+                        name="alamat"
+                        label="Alamat"
+                        rows="3"
+                    />
                 </div>
             </div>
         </div>
